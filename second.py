372632822c2c521b4bd67e48b34f5d1b2de3dc9a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-10 09:01:09 +0330

    first commit

---
+++ second.py
@@ -0,0 +1,46 @@
+
+# primitive dat types in python:
+# number data type
+
+# x = 1
+
+# print(type(x))
+
+# y = 1.5
+# print(type(y))
+
+# output = x + y
+# print("x + y =", output)
+# output = x - y
+# print("x - y =", output)
+# output = x * y
+# print("x * y =", output)
+# output = x / y
+# print("x / y =", output)
+
+# #   bool data type
+# status = True
+# print(type(status))
+# status = False
+# print(type(status))
+
+
+#  string data type
+
+# name = 'martin'
+# print(type(name))
+# name = "martin"
+# print(type(name))
+
+# how to naming variables in python
+
+# هرترکیبی از حروف الفبای انگلیسی، اعداد و _
+# اولین کاراکتر نمی تواند عدد باشد
+number1 = 1
+number2 = 10
+
+number_1 = 3
+_number = 4
+# 4number = 5  # syntax error
+
+# number one = 5  # syntax error
