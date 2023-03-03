81c07bcd0d1a1e21b29d0ccc70e23b0a44f54327
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-03 08:59:43 +0330

    sadeghi

--- string1.py
+++ string1.py
@@ -1,30 +1,42 @@
-print("hello every body. \nwelcome to our python class. \nwe are going to learn everythings about python from zero to hero!!!!")
-print()
-print("""hello every body.
-welcome to our python class.
-we are going to learn everythings about python from zero to hero!!!!""")
-print("id\tname")
-print("01\tartin")
-print("02\ttina")
-score = 0
-score = score + 1
-print("new score is:", score)
-score += 1
-print("new score is:", score)
-print()
-score = score - 1
-print("new score is:", score)
-score -= 1
-print("new score is:", score)
-message = "score\t%s" % score
-print(message)
-message = "name\tfamily\taverage"
-name = input('enter your name: ')
-family = input('enter your family: ')
-average = input('enter your average: ')
-print(message)
-print("%s\t%s\t%s" % (name, family, average))
+# print("hello every body. \nwelcome to our python class. \nwe are going to learn everythings about python from zero to hero!!!!")
+# print()
+# print("""hello every body.
+# welcome to our python class.
+# we are going to learn everythings about python from zero to hero!!!!""")
+# print("id\tname")
+# print("01\tartin")
+# print("02\ttina")
+# score = 0
+# score = score + 1
+# print("new score is:", score)
+# score += 1
+# print("new score is:", score)
+# print()
+# score = score - 1
+# print("new score is:", score)
+# score -= 1
+# print("new score is:", score)
+# message = "score\t%s" % score
+# print(message)
+# message = "name\tfamily\taverage"
+# name = input('enter your name: ')
+# family = input('enter your family: ')
+# average = input('enter your average: ')
+# print(message)
+# print("%s\t%s\t%s" % (name, family, average))
 
 
 # exercise 1: برنامه ای بنویسدی که نام ، نام خانوادگی و سن را از ورودی دریافت کند  . به صورت زیر نمایش دهد
 # ali rezaei is 10 years old
+
+# name = input('enter your name: ')
+# family = input('enter your family: ')
+# age = input('enter your age: ')
+# message = f"{name} {family} is {age} years old."
+# print(message)
+
+# message = 'ali\'s age is 10'
+# message = "ali's age is 10"
+# message = '''ali's age is 10'''
+# message = """ali's age is 10"""
+message = He said, "Aren't can't shouldn't wouldn't."
---
+++ string2.py
@@ -0,0 +1,53 @@
+# message = He said, "Aren't can't shouldn't wouldn't."
+
+# str1 = "arman"
+# str2 = "rezaei"
+
+# output = str1 + " " + str2
+
+# print(output)
+# str1 = "arman"
+# str2 = 1
+
+# output = str1 + " " + str2 # error
+# output = str1 + " " + str(str2)
+
+# print(output)
+
+
+# str1 = '*'
+# print(str1 * 10)
+
+# number1 = int(input('enter first number: '))
+# print(type(number1))
+# number2 = int(input('enter second number: '))
+# print(type(number1))
+
+# print(f'{number1} + {number2} = {number1 + number2}')
+number1 = float(input('enter first number: '))
+print(type(number1))
+number2 = float(input('enter second number: '))
+print(type(number1))
+
+print(f'{number1} + {number2} = {number1 + number2}')
+
+
+# exercise 1 : برنامه ای بنویسید که نام، تام خانوادگی و نمرات سه درس یک دانش آموز را
+# از ورودی دریافت نماید
+# و معدل او را محاسبه و به صورت زیر نمایش دهد.
+# reza sadeghi's average is 95
+
+# exercise 2 : برنامه ای بنویسید که یک عدد را از وزودی دریافت نماید
+# و به تعداد آن کاراکتر شارپ را پرینت نماید.
+
+# exercise 3 : F = m * a
+# برنامه ای بنویسید که جرم و شتاب را از ورودی دریافت نماید و نیرو را محاسبه نماید
+# F = نیرو
+# m = جرم
+# a = شتاب
+
+# exercise 4:
+# برنامه ای بنویسید که سه عدد از ورودی دریافت نماید
+# و عدد اول را بر عدد دوم تقسیم نماید و با عدد سوم جمع نماید
+# در نهایت حاصل را با عدد یک جمع نماید
+# حاصل را پرینت کنید.
