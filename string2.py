307b0f0e4070c9298fe27ff1e4cb625888cf7e64
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-10 09:01:07 +0330

    sadeghi

--- string2.py
+++ string2.py
@@ -24,12 +24,12 @@
 # print(type(number1))
 
 # print(f'{number1} + {number2} = {number1 + number2}')
-number1 = float(input('enter first number: '))
-print(type(number1))
-number2 = float(input('enter second number: '))
-print(type(number1))
+# number1 = float(input('enter first number: '))
+# print(type(number1))
+# number2 = float(input('enter second number: '))
+# print(type(number1))
 
-print(f'{number1} + {number2} = {number1 + number2}')
+# print(f'{number1} + {number2} = {number1 + number2}')
 
 
 # exercise 1 : برنامه ای بنویسید که نام، تام خانوادگی و نمرات سه درس یک دانش آموز را
@@ -51,3 +51,50 @@ print(f'{number1} + {number2} = {number1 + number2}')
 # و عدد اول را بر عدد دوم تقسیم نماید و با عدد سوم جمع نماید
 # در نهایت حاصل را با عدد یک جمع نماید
 # حاصل را پرینت کنید.
+
+
+# n1 = float(input('enter first number:> '))
+# n2 = float(input('enter second number:> '))
+# n3 = float(input('enter third number:> '))
+
+# print("result is:", n1/n2 + n3 + 1)
+# print(f'{n1}/{n2} + {n3} + 1 = {n1/n2 + n3 + 1}')
+
+# x1 = 2
+# x2 = 3
+# x3 = 2
+
+# print(f'{x1} ÷ {x2} × {x3} = {x1/x2 * x3}')
+
+# print('hello.\nwelcome')
+# print('''hello
+# welcome''')
+
+# name = "ahura"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])# error
+# ex1
+# برنامه ای بنویسید که نام کاربر را از از ورودی بگیرد و کاراکترهای آن را با فاصله کنار هم نمایش دهد.
+
+# ex2 برنامه ای بنویسید که نام کاربر را از ورودی بگیرد و کاراکترهای آن را از انتها به ابتدا نمایش دهد.
+
+# ex3
+string = '123456789'
+# برنامه ای بنویسید که مجموع اعداد رشته بالا را محاسبه و پرینت نماید.
+print(string[0] + string[1])  # باید چکار کنیم؟
+
+# برش زدن روی رشته ها
+string = 'abcdefghi'
+print(string[0:3])  # abc
+print(string[3:6])  # def
+
+# ex3 برنامه ای بنویسید که برش های زیر را از رشته بالا ایجاد نماید
+# ghi
+# efg
+# cde
+# bcd
+# cdef
