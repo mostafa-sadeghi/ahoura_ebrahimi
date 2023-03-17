4c255ab95896605e988d41994b733757177ab032
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-17 09:02:56 +0330

    sadeghi

---
+++ list1.py
@@ -0,0 +1,39 @@
+# shoppingList = ""
+# '''
+# 1. egg
+# 2. apple
+# 3. banana
+# '''
+# product = "mashroom"
+# shoppingList = shoppingList + product  # shoppingList += product
+
+# product2 = "apple"
+# shoppingList = shoppingList + '\n' + product2
+
+# print(shoppingList)
+
+# print(shoppingList[0])
+
+
+# shoppingList = "mashroom\napple"
+# print()  # apple
+# print()  # mashroom
+
+
+shopping_list = []
+
+shopping_list.append("apple")
+shopping_list.append("banana")
+shopping_list.append("mashroom")
+
+print(shopping_list)
+print("1.", shopping_list[0])
+print("2.", shopping_list[1])
+print("3.", shopping_list[2])
+
+# برنامه ای بنویسید که نام چهار دانش آموز را از ورودی دریافت نماید و در لیستی ذخیره کند
+# از لیست بالا اولین اسم و آخرین اسم را جداگانه پرینت کنید
+
+
+# برنامه ای بنویسید که چهار عدد را از ورودی دریافت نماید و در لیست ذخیره کند
+# مجموع اعداد لیست را پرینت نماید.
