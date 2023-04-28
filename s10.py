5b6ebf8c9bf7d76edaf9383544cd5af5aed7a968
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-28 08:58:35 +0330

    sadeghi

---
+++ s10.py
@@ -0,0 +1,33 @@
+# for i in [2,4,6,7,4,2]:
+#     print(i)
+
+# for s in 'python':
+#     print(s.upper(), end=' ')
+
+# print()
+
+# string = 'python'
+# print(string[:3])
+
+# برنامه ای بنویسید که نا م کاربر را از ورودی دریافت نماید
+# و تک تک کارکترهای آن را با یک فاصله و با حروف بزرگ نمایش دهد
+
+
+# exercise 1:
+# MONTHS = "JanFebMarAprMay"
+# کاربر عددی را وارد می نماید و برنامه می بایست اسم ماه را نمایش دهد.
+# مثال:
+# user input = 1    => output : Jan 
+
+
+# for i in range(3):
+#     print("a")
+
+# for j in range(3):
+#     print("b")
+
+# for i in range(3):
+#     print("a")
+
+#     for j in range(3):
+#         print("b")
