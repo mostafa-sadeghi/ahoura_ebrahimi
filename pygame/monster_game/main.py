4ae236902f5f137987d38b876d2d42872e8a7c0c
msa <msai.sadeghi@gmail.com>
2023-09-21 16:21:45 +0330

    s

---
+++ pygame/monster_game/main.py
@@ -0,0 +1,16 @@
+import pygame
+from config import *
+pygame.init()
+
+
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+    clock.tick(FPS)
